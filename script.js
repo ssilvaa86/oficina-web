a817e33e62be3b563bd88b36cd6ede88d2f11da4
ssilvaa86 <stephanie.silva.aliaga@gmail.com>
2026-03-12 20:16:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let images = []
-let current = 0
+let images=[]
+let current=0
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded",()=>{
 
-images = Array.from(document.querySelectorAll(".gallery img"))
+images=Array.from(document.querySelectorAll(".gallery img"))
 
 images.forEach((img,index)=>{
 img.addEventListener("click",()=>{
@@ -15,12 +15,13 @@ openImage(index)
 
 function openImage(index){
 
-current = index
+current=index
 
-const lightbox = document.getElementById("lightbox")
-const lightboxImg = document.getElementById("lightbox-img")
+const lightbox=document.getElementById("lightbox")
+const lightboxImg=document.getElementById("lightbox-img")
 
-lightboxImg.src = images[current].src
+lightboxImg.src=images[current].src
+
 lightbox.style.display="flex"
 
 }
@@ -33,11 +34,11 @@ function nextImage(){
 
 current++
 
-if(current >= images.length){
-current = 0
+if(current>=images.length){
+current=0
 }
 
-document.getElementById("lightbox-img").src = images[current].src
+document.getElementById("lightbox-img").src=images[current].src
 
 }
 
@@ -45,19 +46,10 @@ function prevImage(){
 
 current--
 
-if(current < 0){
-current = images.length-1
+if(current<0){
+current=images.length-1
 }
 
-document.getElementById("lightbox-img").src = images[current].src
-
-}
-
-function openWhatsApp(){
-
-window.open(
-"https://wa.me/56987475287?text=Hola%2C%20me%20interesa%20la%20oficina%20205%20en%20Alameda%204050",
-"_blank"
-)
+document.getElementById("lightbox-img").src=images[current].src
 
 }
