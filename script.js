fbf0a869dfbdfe1e69691ccc2804251b93b77ec4
ssilvaa86 <stephanie.silva.aliaga@gmail.com>
2026-03-12 19:56:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function openWhatsApp(){
+
+window.open(
+"https://wa.me/56987475287?text=Hola%2C%20me%20interesa%20la%20oficina%20205%20en%20Alameda%204050.%20%C2%BFPodr%C3%ADan%20enviarme%20m%C3%A1s%20informaci%C3%B3n%3F",
+"_blank"
+);
+
+}
+
+function openMaps(){
+
+window.open(
+"https://maps.app.goo.gl/NJcWChhAF48HtTVv9",
+"_blank"
+);
+
+}
